3ad42ef5326aa572c34526550f22fefe87d7e511
dvhar <dh22@protonmail.com>
2021-02-16 09:41:12 -0600

    fix install and kill ueberzug

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 
 go build
 sudo mkdir -p /opt/lf /etc/lf
-cp ./previews/lfrc /etc/lf/
+sudo cp ./previews/lfrc /etc/lf/
 sudo cp lf /opt/lf/
 sudo cp ./previews/pvimg.sh /opt/lf/
 sudo cp ./previews/preview-image /opt/lf/
