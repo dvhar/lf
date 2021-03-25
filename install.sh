cf6789aec76e901bffbef0cbf02a22e1ed4af16a
dvhar <dh22@protonmail.com>
2021-03-24 22:31:57 -0500

    update previewer with upstream

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ go build
 sudo mkdir -p /opt/lf /etc/lf
 sudo cp -u ./previews/lfrc /etc/lf/
 sudo cp -u lf /opt/lf/
-sudo cp -u ./previews/pvimg.sh /opt/lf/
-sudo cp -u ./previews/preview-image /opt/lf/
+sudo cp -u ./previews/preview /opt/lf/
+sudo cp -u ./previews/cleaner /opt/lf/
 sudo cp -u ./previews/lf /usr/local/bin/lf
